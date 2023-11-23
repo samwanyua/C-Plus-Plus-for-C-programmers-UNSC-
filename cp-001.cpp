e57e31e2e7dfb6dceaf8a8a9ac3db55b111ec1f3
samwanyua <samexample8@gmail.com>
2023-11-23 22:36:33 +0300

    Converting c program to c++ intro

---
+++ cp-001.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace :: std;
+
+/*
+**C PROGRAM
+
+srand(clock());
+printf("\nENter the number of trials: ");
+scanf("%d", &trials);
+for(j = 0; k < trials; ++j)
+    outcomes[(d1 = R_SIDE) + (d2 = R_SIDE)]++;
+printf("probability\n");
+
+for(j = 2; j < n_dice *SIDES + 1; ++j)
+    printf(" j = %d p = %lf\n", j,
+    (double) (outocomes[j])/trials);
+    
+
+
+
+
+*/
