f4a9d6b66a46670e8b501226e6c6886fdc9cfe76
samwanyua <samexample8@gmail.com>
2023-11-23 23:36:24 +0300

    Using C++ Code

---
+++ cp-002.cpp
@@ -0,0 +1,11 @@
+/*Using C++ Code*/
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace :: std;
+
+const int sides = 6;
+inline int r_sides(){return (rand() % sides + 1);} //inline is a compiler directive
+//inline is a keyword
+
